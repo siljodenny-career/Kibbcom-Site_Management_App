3fbb24e3bde77507cd738e91b99ad117bc0f0db2
Siljo Denny <siljodennyjohn>
2026-05-16 14:30:49 +0530

    Add DevicePreview for Laptop Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,45 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:site_dashboard/view/screens/splash_screen.dart';
 import 'view/theme/app_theme.dart';
 
 void main() {
+
+    WidgetsFlutterBinding.ensureInitialized();
+
+  // ── Get real physical screen width before app builds ──
+  final view = WidgetsBinding.instance.platformDispatcher.views.first;
+  final physicalWidth = view.physicalSize.width;
+  final devicePixelRatio = view.devicePixelRatio;
+  final logicalWidth = physicalWidth / devicePixelRatio;
+
   runApp(
-    ProviderScope(
-      child: DevicePreview(
-        enabled: true,
-        backgroundColor: Colors.black,
-        builder: (context) => const SiteDashboardApp(),
-      ),
+    DevicePreview(
+      enabled: _shouldEnableDevicePreview(logicalWidth),
+      builder: (context) => const ProviderScope(child: SiteDashboardApp()),
     ),
   );
+}
+
+bool _shouldEnableDevicePreview(double logicalWidth) {
+  // Never show on native mobile platforms regardless of screen size
+  if (!kIsWeb) {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+      case TargetPlatform.iOS:
+      case TargetPlatform.fuchsia:
+        return false;
+
+      case TargetPlatform.windows:
+      case TargetPlatform.macOS:
+      case TargetPlatform.linux:
+        return true;
+    }
+  }
+  //LAptop Screen size
+  return logicalWidth >= 1024;
 }
 
 class SiteDashboardApp extends StatelessWidget {
@@ -23,11 +49,11 @@ class SiteDashboardApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Site Dashboard',
-
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       theme: AppTheme.dark,
+      //Required for DevicePreview to work properly
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       home: const SplashScreen(),
     );
   }
